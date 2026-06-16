6aa2ecc7b5eaf2860c6e570739b7a1fc8652c26c
anna <165697505+annsamsonann@users.noreply.github.com>
2026-06-16 10:55:47 -0400

    Update generatePassiveCommands.m

--- generatePassiveCommands.m
+++ generatePassiveCommands.m
@@ -11,23 +11,22 @@ function [new_speed_fast, new_acc_fast, new_dist_fast, ...
     cmdSlow_cm_s = avgSlow;
 
     % % Convert to step-domain motor commands
-    % new_speed_fast = round(cmdFast_cm_s / cm_per_step);          % steps/s
-    % new_speed_slow = round(cmdSlow_cm_s / cm_per_step);          % steps/s
-
-    % new_acc_fast   = round(new_speed_fast * 1000);               % stage accel units used in your test
-    % new_acc_slow   = round(new_speed_slow * 1000);
-
-    % new_dist_fast  = round((cmdFast_cm_s * t) / cm_per_step);    % steps
-    % new_dist_slow  = round((cmdSlow_cm_s * t) / cm_per_step);    % steps
+    new_speed_fast = round(cmdFast_cm_s / cm_per_step);          % steps/s
+    new_speed_slow = round(cmdSlow_cm_s / cm_per_step);          % steps/s
+ 
+    new_acc_fast   = round(new_speed_fast * 1000);               % stage accel units used in your test
+    new_acc_slow   = round(new_speed_slow * 1000);
+    new_dist_fast  = round((cmdFast_cm_s * t) / cm_per_step);    % steps
+    new_dist_slow  = round((cmdSlow_cm_s * t) / cm_per_step);    % steps
 
 
      % TEST
-     new_speed_fast = 50000;          % steps/s
-     new_speed_slow = 10;          % steps/s
- 
-     new_acc_fast   = 50000;             % stage accel units used in your test
-     new_acc_slow   = 10;
- 
-     new_dist_fast  = 50000;    % steps
-     new_dist_slow  = 10;    % steps
+     % new_speed_fast = 50000;          % steps/s
+     % new_speed_slow = 10;          % steps/s
+     % 
+     % new_acc_fast   = 50000;             % stage accel units used in your test
+     % new_acc_slow   = 10;
+     % 
+     % new_dist_fast  = 50000;    % steps
+     % new_dist_slow  = 10;    % steps
 end
