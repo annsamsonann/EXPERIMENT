de142716597358d5d83a77b516b9331150df6f7e
annsamsonann <165697505+annsamsonann@users.noreply.github.com>
2026-06-17 13:37:17 -0400

    added thresholds for trials used for avg speeds

--- generatePassiveCommands.m
+++ generatePassiveCommands.m
@@ -3,18 +3,29 @@ function [new_speed_fast, new_acc_fast, new_dist_fast, new_speed_slow, new_acc_s
       isFast = activeBehavior.Arm_Mov_Acc_arduino == fastMotionCommanded;
       isSlow = activeBehavior.Arm_Mov_Acc_arduino == slowMotionCommanded;
 
-      avgFast = mean(activeBehavior.MeasuredSpeed_cm_s(isFast), 'omitnan');
-      avgSlow = mean(activeBehavior.MeasuredSpeed_cm_s(isSlow), 'omitnan');
-      % Discard trials that are 
-            %we should choose the fast speed so that fast speed is less than max speed by 20 % 
-                  %max speed defined by dist from one end to the other is 
-                  %max speed from the middle is: ? 
 
-      t = 1.75;              % movement duration in sec
-      cm_per_step = 0.00467;
-      range_cm = 17.145;
-      max_steps = range_cm /cm_per_step;
-      max_speed = range_cm/t;
+      % Discard trials that are over max speed or thresholds
+      fast_upper_speed_threshold = ExpConfig.max_speed_cm;
+      fast_lower_speed_threshold = ExpConfig.fast_target * 0.8;
+      
+      slow_upper_speed_threshold = ExpConfig.slow_target * 1.2;
+      slow_lower_speed_threshold = ExpConfig.slow_target * 0.8;
+      
+      % logical masks over all trials
+      FastTrialsGoodMask = isFast ...
+          & activeBehavior.MeasuredSpeed_cm_s <= fast_upper_speed_threshold ...
+          & activeBehavior.MeasuredSpeed_cm_s >= fast_lower_speed_threshold;
+      
+      SlowTrialsGoodMask = isSlow ...
+          & activeBehavior.MeasuredSpeed_cm_s <= slow_upper_speed_threshold ...
+          & activeBehavior.MeasuredSpeed_cm_s >= slow_lower_speed_threshold;
+      
+      % extract the speeds that pass the criteria
+      FastTrialsGood  = activeBehavior.MeasuredSpeed_cm_s(FastTrialsGoodMask);
+      SlowTrialsGood  = activeBehavior.MeasuredSpeed_cm_s(SlowTrialsGoodMask);
+      
+      avgFast = mean(FastTrialsGood, 'omitnan');
+      avgSlow = mean(SlowTrialsGood, 'omitnan');
 
       % In your latest calibration: commanded == observed
       cmdFast_cm_s = avgFast;
