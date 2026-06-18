65f1fdd0cd8bc0897b5c80e753ba22571ff16696
annsamsonann <165697505+annsamsonann@users.noreply.github.com>
2026-06-18 01:32:18 -0400

    Update generatePassiveCommands.m

--- generatePassiveCommands.m
+++ generatePassiveCommands.m
@@ -1,12 +1,12 @@
 %return UNSIGNED 
-function [new_speed_fast, new_acc_fast, new_dist_fast, new_speed_slow, new_acc_slow, new_dist_slow] = generatePassiveCommands(activeBehavior,fastMotionCommanded,slowMotionCommanded)
+function [avgFast,avgSlow,new_speed_fast, new_acc_fast, new_dist_fast, new_speed_slow, new_acc_slow, new_dist_slow] = generatePassiveCommands(activeBehavior,fastMotionCommanded,slowMotionCommanded)
       isFast = activeBehavior.Arm_Mov_Acc_arduino == fastMotionCommanded;
       isSlow = activeBehavior.Arm_Mov_Acc_arduino == slowMotionCommanded;
       cm_per_step = ExpConfig.cm_per_step;
-      t           = ExpConfig.StageMotionDur_sec;
+      t = ExpConfig.StageMotionDur_sec;
 
 
-      % Discard trials that are over max speed or thresholds
+      % Discard trials that are over max speed or over the thresholds
       fast_upper_speed_threshold = ExpConfig.max_speed_cm;
       fast_lower_speed_threshold = ExpConfig.fast_target * 0.8;
       
@@ -29,20 +29,19 @@ function [new_speed_fast, new_acc_fast, new_dist_fast, new_speed_slow, new_acc_s
       avgFast = mean(FastTrialsGood, 'omitnan');
       avgSlow = mean(SlowTrialsGood, 'omitnan');
 
-      % In your latest calibration: commanded == observed
-      cmdFast_cm_s = avgFast;
-      cmdSlow_cm_s = avgSlow;
 
       % % Convert to step-domain motor commands
-      new_speed_fast = round(cmdFast_cm_s / cm_per_step);          % steps/s
-      new_speed_slow = round(cmdSlow_cm_s / cm_per_step);          % steps/s
+      new_speed_fast = round(avgFast / cm_per_step);          % steps/s
+      new_speed_slow = round(avgSlow / cm_per_step);          % steps/s
       
       new_acc_fast   = round(new_speed_fast * 1000);               % stage accel units used in your test
       new_acc_slow   = round(new_speed_slow * 1000);
-      new_dist_fast  = round((cmdFast_cm_s * t) / cm_per_step);    % steps
-      new_dist_slow  = round((cmdSlow_cm_s * t) / cm_per_step);    % steps
+      new_dist_fast  = round((avgFast * t) / cm_per_step);    % steps
+      new_dist_slow  = round((avgSlow * t) / cm_per_step);    % steps
+
+      %to check
     nFastInRange = sum(FastTrialsGoodMask);
-nSlowInRange = sum(SlowTrialsGoodMask);
+    nSlowInRange = sum(SlowTrialsGoodMask);
     if nFastInRange == 0
         fprintf('FAST avg is NaN because no fast trials passed the threshold.\n');
     end
